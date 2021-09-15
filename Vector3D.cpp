20ebeb62ec4a357b0e78a2eb8da67464c84a2700
robin <robin.galmiche@yahoo.fr>
2021-09-15 14:47:09 -0400

    Implement Vector3D class

---
+++ Vector3D.cpp
@@ -0,0 +1,8 @@
+#include "Vector3D.h"
+
+Vector3D::Vector3D(float x, float y, float z)
+{
+	this->x= x;
+	this->y = y;
+	this->z = z;
+}
---
+++ Vector3D.h
@@ -0,0 +1,13 @@
+#pragma once
+class Vector3D
+{
+	public:
+		float x;
+		float y;
+		float z;
+		Vector3D(float, float, float);
+
+
+
+};
+
